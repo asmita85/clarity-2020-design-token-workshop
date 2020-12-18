0f4bc62010a0368c1de754af29e8395863578cc2
Asma Charef <asma.charef@hotmail.com>
2020-12-18 15:21:53 -0600

    first sass file

--- build.js
+++ build.js
@@ -1,6 +1,9 @@
+//import the filessystem module
 const fs = require('fs');
+//import the data structur json 
 const tokens = require('./tokens.json');
 console.log(tokens);
+//transfor the tokens into a shape that looks like a saas variable
 const transformedTokens = Object.keys(tokens).map(key => {
     return {
         name: `$${key}`,
@@ -8,9 +11,10 @@ const transformedTokens = Object.keys(tokens).map(key => {
     }
 });
 console.log(transformedTokens);
+//format the transformed tokens to variable file
 const formattedTokens = transformedTokens.map(token => {
     return `${token.name}: ${token.value};`
 }).join(`\n`);
 console.log(formattedTokens);
-
+// create variable.scss file 
 fs.writeFileSync('dist/variables.scss', formattedTokens);
